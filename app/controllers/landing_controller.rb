8f67ef713d70a5ad0b3507913468d9c68056e60f
Charles Clemens <cclemensjr@gmail.com>
2018-06-07 06:18:53 -0700

    Rendered choreson the landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,8 @@
 class LandingController < ApplicationController
   def index
     @total_chores = Chore.count
+    @chores = Chore.all
+    @random_chore = Chore.randomize_chore
+    @user = User.find(params[:id])
   end
 end
