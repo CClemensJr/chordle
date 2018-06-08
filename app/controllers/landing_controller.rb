1391b0370da6ce4a3ead8c6dd39d52a009ede6f7
Charles Clemens <c@Charless-MacBook-Pro.local>
2018-06-08 12:39:48 -0700

    Added stuff

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,13 @@
 class LandingController < ApplicationController
   def index
-    @total_chores = Chore.count
-    @chores = Chore.all
-    @random_chore = Chore.randomize_chore
     @user = User.find(params[:id])
+    
+    if Chore.nil?
+      redirect_to new_chore_path
+    else
+      @total_chores = Chore.count
+      @chores = Chore.all
+      @random_chore = Chore.randomize_chore
+    end
   end
 end
